ce993c1958cc7de05481499fc8bc2c27fc17b597
Nikita <klerik1995@mail.ru>
2019-05-01 15:25:16 +0300

    CreateActorService

--- src/main/java/spring/nikitasterlit/service/ActorService.java
+++ src/main/java/spring/nikitasterlit/service/ActorService.java
@@ -1,14 +1,34 @@
 package spring.nikitasterlit.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import spring.nikitasterlit.model.Actor;
+import spring.nikitasterlit.repository.ActorRepository;
 
 import java.util.List;
 
-public interface ActorService {
-    Actor addActor(Actor actor);
-    void deleteBank(long id);
-    Actor editActor(Actor actor);
-    Actor findById(long id);
-    List<Actor> findAll();
+public class ActorService {
+    @Autowired
+    ActorRepository actorRepository;
+
+    Actor addActor(Actor actor){
+        return actorRepository.save(actor);
+    }
+    void deleteBank(long id){
+        Actor actor = actorRepository.findById(id).get();
+        actorRepository.delete(actor);
+    }
+    Actor editActor(Actor actor){
+        Actor actor1 = actorRepository.findById(actor.getId()).get();
+        actor1.setFirstName(actor.getFirstName());
+        actor1.setLastName(actor.getLastName());
+        actor1.setGenre(actor.getGenre());
+        return actorRepository.save(actor1);
+    }
+    Actor findById(long id){
+        return actorRepository.findById(id).get();
+    }
+    List<Actor> findAll(){
+     return actorRepository.findAll();
+    }
 }
